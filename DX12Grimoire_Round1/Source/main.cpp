3d79a044d684cd9a8fbb8afcbafd7e12918931c8
hitsuji1970 <46307808+hitsuji1970@users.noreply.github.com>
2020-02-23 18:33:22 +0900

    コマンドリストとスワップチェーンの作成

--- DX12Grimoire_Round1/Source/main.cpp
+++ DX12Grimoire_Round1/Source/main.cpp
@@ -1,10 +1,18 @@
+#include <vector>
+#include <d3d12.h>
+#include <dxgi1_6.h>
 #include <Windows.h>
 #include <tchar.h>
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
 #ifdef _DEBUG
 #include <iostream>
 #endif
-
 using namespace std;
+
+HWND InitWindow(WNDCLASSEX* const pWndClass);
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 constexpr unsigned int window_width = 1280;
 constexpr unsigned int window_height = 720;
@@ -19,16 +27,6 @@ void DebugOutputFromString(const char* format, ...)
 #endif
 }
 
-LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-	if (msg == WM_DESTROY) {
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-}
-
 #ifdef _DEBUG
 int main()
 #else
@@ -38,27 +36,82 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	DebugOutputFromString("Show window test.");
 
 	WNDCLASSEX w = {};
-	w.cbSize = sizeof(WNDCLASSEX);
-	w.lpfnWndProc = WNDPROC(WindowProcedure);
-	w.lpszClassName = _T("DX12Sample");
-	w.hInstance = GetModuleHandle(nullptr);
+	HWND hWnd = InitWindow(&w);
 
-	RegisterClassEx(&w);
+	ID3D12Device* _dev = nullptr;
+	IDXGIFactory6* _dxgiFactory = nullptr;
+	IDXGISwapChain4* _swapchain = nullptr;
 
-	RECT wrc = { 0, 0, window_width, window_height };
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	D3D_FEATURE_LEVEL levels[] = {
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+	D3D_FEATURE_LEVEL featureLevel;
 
-	HWND hWnd = CreateWindow(w.lpszClassName,
-		_T("DX12テスト"),
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		wrc.right - wrc.left,
-		wrc.bottom - wrc.top,
-		nullptr,
-		nullptr,
-		w.hInstance,
-		nullptr);
+#ifdef _DEBUG
+	auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+	std::vector<IDXGIAdapter*> adapters;
+	IDXGIAdapter* tmpAdapter = nullptr;
+	for (int i = 0; _dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND; i++) {
+		adapters.push_back(tmpAdapter);
+	}
+	for (auto adpt : adapters) {
+		DXGI_ADAPTER_DESC adesc = {};
+		adpt->GetDesc(&adesc);
+		std::wstring strDesc = adesc.Description;
+		if (strDesc.find(L"NVIDIA") != std::string::npos) {
+			tmpAdapter = adpt;
+			break;
+		}
+	}
+#endif
+
+	for (auto lv : levels) {
+		if (D3D12CreateDevice(nullptr, lv, IID_PPV_ARGS(&_dev)) == S_OK) {
+			featureLevel = lv;
+			break;
+		}
+	}
+
+	if (!_dev) {
+		::MessageBox(hWnd, _T("Error"), _T("D3Dデバイスの初期化に失敗しました。"), MB_ICONERROR);
+		exit(-1);
+	}
+
+	// コマンドリスト
+	ID3D12CommandAllocator* _cmdAllocator = nullptr;
+	ID3D12GraphicsCommandList* _cmdList = nullptr;
+	result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&_cmdAllocator));
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, _cmdAllocator, nullptr, IID_PPV_ARGS(&_cmdList));
+
+	ID3D12CommandQueue* _cmdQueue = nullptr;
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	cmdQueueDesc.NodeMask = 0;
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&_cmdQueue));
+
+	// スワップチェーン
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+	swapChainDesc.Width = window_width;
+	swapChainDesc.Height = window_height;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.Stereo = false;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+	swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	result = _dxgiFactory->CreateSwapChainForHwnd(
+		_cmdQueue, hWnd, &swapChainDesc, nullptr, nullptr,
+		(IDXGISwapChain1**)&_swapchain
+	);
 
 	ShowWindow(hWnd, SW_SHOW);
 
@@ -79,4 +132,42 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	return 0;
 }
+
+HWND InitWindow(WNDCLASSEX* const pWndClass)
+{
+	pWndClass->cbSize = sizeof(WNDCLASSEX);
+	pWndClass->lpfnWndProc = WNDPROC(WindowProcedure);
+	pWndClass->lpszClassName = _T("DX12Sample");
+	pWndClass->hInstance = GetModuleHandle(nullptr);
+
+	RegisterClassEx(pWndClass);
+
+	RECT wrc = { 0, 0, window_width, window_height };
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hWnd = CreateWindow(pWndClass->lpszClassName,
+		_T("DX12テスト"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		pWndClass->hInstance,
+		nullptr);
+
+	return hWnd;
+}
+
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	if (msg == WM_DESTROY) {
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
 
