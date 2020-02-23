0d0036859db72e3524e9eb2d3c5a7493a53220c6
hitsuji1970 <46307808+hitsuji1970@users.noreply.github.com>
2020-02-23 16:40:30 +0900

    最初のウィンドウ表示

---
+++ DX12Grimoire_Round1/Source/main.cpp
@@ -0,0 +1,82 @@
+#include <Windows.h>
+#include <tchar.h>
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+using namespace std;
+
+constexpr unsigned int window_width = 1280;
+constexpr unsigned int window_height = 720;
+
+void DebugOutputFromString(const char* format, ...)
+{
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	printf(format, valist);
+	va_end(valist);
+#endif
+}
+
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	if (msg == WM_DESTROY) {
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+#ifdef _DEBUG
+int main()
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+#endif
+{
+	DebugOutputFromString("Show window test.");
+
+	WNDCLASSEX w = {};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = WNDPROC(WindowProcedure);
+	w.lpszClassName = _T("DX12Sample");
+	w.hInstance = GetModuleHandle(nullptr);
+
+	RegisterClassEx(&w);
+
+	RECT wrc = { 0, 0, window_width, window_height };
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hWnd = CreateWindow(w.lpszClassName,
+		_T("DX12ƒeƒXƒg"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		w.hInstance,
+		nullptr);
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	MSG msg = {};
+
+	while (true) {
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT) {
+			break;
+		}
+	}
+
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
+	return 0;
+}
+
