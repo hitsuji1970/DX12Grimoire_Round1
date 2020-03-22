9a5885a1802bbdd143c0100b87c066190548d446
hitsuji1970 <46307808+hitsuji1970@users.noreply.github.com>
2020-03-22 17:46:05 +0900

    ピクセルシェーダーでテクスチャ－を参照

--- Chapter05/Source/BasicPixelShader.hlsl
+++ Chapter05/Source/BasicPixelShader.hlsl
@@ -2,5 +2,6 @@
 
 float4 BasicPS(VSOutput input) : SV_TARGET
 {
-	return float4(input.uv, 1, 1);
+	//return float4(input.uv, 1, 1);
+	return float4(tex.Sample(smp, input.uv));
 }
--- Chapter05/Source/BasicShaderHeader.hlsli
+++ Chapter05/Source/BasicShaderHeader.hlsli
@@ -1,3 +1,6 @@
+Texture2D<float4> tex : register(t0);
+SamplerState smp : register(s0);
+
 struct VSOutput {
 	float4 svpos:SV_POSITION;
 	float2 uv:TEXCOORD;
