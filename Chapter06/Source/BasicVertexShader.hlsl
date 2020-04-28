b7c916ede9e1235848f8eedb516de4800b82645b
hitsuji1970 <46307808+hitsuji1970@users.noreply.github.com>
2020-04-28 22:27:29 +0900

    定数バッファーのレジスター定義をインクルードファイルに移動。

--- Chapter06/Source/BasicVertexShader.hlsl
+++ Chapter06/Source/BasicVertexShader.hlsl
@@ -1,11 +1,5 @@
 #include "BasicShaderHeader.hlsli"
 
-// 定数バッファー
-cbuffer cbuff0 : register(b0)
-{
-	matrix mat;
-};
-
 VSOutput BasicVS(float4 pos : POSITION, float2 uv : TEXCOORD)
 {
 	VSOutput output;
