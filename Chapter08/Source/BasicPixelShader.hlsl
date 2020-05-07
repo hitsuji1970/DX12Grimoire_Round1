c6199e76f0175dd9ca4c6922b59d124e37460a09
hitsuji1970 <46307808+hitsuji1970@users.noreply.github.com>
2020-05-07 11:39:30 +0900

    アンビエントカラーを適用

--- Chapter08/Source/BasicPixelShader.hlsl
+++ Chapter08/Source/BasicPixelShader.hlsl
@@ -2,12 +2,14 @@
 
 float4 BasicPS(VSOutput input) : SV_TARGET
 {
+	float4 color = tex.Sample(smp, input.uv);
 	float3 light = normalize(float3(1, -1, 1));
 	float brightness = dot(-light, input.normal.xyz);
 	float2 sphereMapUV = (input.vnormal.xy + float2(1, -1)) * float2(0.5, -0.5);
 	return float4(brightness, brightness, brightness, 1)
 		* diffuse
-		* tex.Sample(smp, input.uv)
+		* color
 		* sph.Sample(smp, sphereMapUV)
-		+ spa.Sample(smp, sphereMapUV);
+		+ spa.Sample(smp, sphereMapUV)
+		+ float4(color * ambient, 1);
 }
